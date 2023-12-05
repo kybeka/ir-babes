52cafcc5c9274f395e034a15a9c8040a751d56a3
kyla kaplan <kaplak@usi.ch>
2023-12-05 16:19:33 +0100

    setting up with vuetify now

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld/>
+      <!-- <HelloWorld/> -->
+      <SearchComponentVue/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import SearchComponentVue from './components/SearchComponent.vue'
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    // HelloWorld,
+    SearchComponentVue
   },
 
   data: () => ({
--- frontend/src/components/SearchComponent.vue
+++ frontend/src/components/SearchComponent.vue
@@ -1,89 +1,58 @@
 <template>
-    <div>
-      <input v-model="query" placeholder="Enter your search query" />
-      <button @click="search">Search</button>
-  
-      <div v-if="results.length">
-        <h2>Search Results</h2>
-        <ul>
-          <li v-for="result in results" :key="result.id">
-            <a :href="result.url" target="_blank">{{ result.title }}</a>
-            <p>{{ result.description }}</p>
-          </li>
-        </ul>
-      </div>
-  
-      <div v-else>
-        <p>No results found for the given query.</p>
-      </div>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        query: '',
-        results: [],
-      };
+  <v-container class="search-container">
+    <v-row justify="center">
+      <v-col cols="12" sm="8" md="6">
+        <v-card>
+          <v-card-title class="title">News d-AI-ly</v-card-title>
+          <v-card-text>
+            <v-text-field v-model="query" label="Enter your search query" outlined></v-text-field>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn @click="search" color="primary">Search</v-btn>
+          </v-card-actions>
+        </v-card>
+
+        <v-divider></v-divider>
+
+        <v-card v-if="results.length" class="results-card">
+          <v-card-title class="results-title">Search Results</v-card-title>
+          <v-list>
+            <v-list-item v-for="result in results" :key="result.id">
+              <v-list-item-content>
+                <v-list-item-title>
+                  <v-btn text :href="result.url" class="result-link">{{ result.title }}</v-btn>
+                </v-list-item-title>
+                <v-list-item-subtitle>{{ result.description }}</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-card>
+
+        <v-card v-else class="results-card">
+          <v-card-title class="results-title">No results found</v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      query: '',
+      results: [],
+    };
+  },
+  methods: {
+    search() {
+      // Simulate search results (replace this with your actual data)
+      this.results = [
+        { id: 1, title: 'Result 1', description: 'Description for Result 1', url: 'https://example.com/result1' },
+        { id: 2, title: 'Result 2', description: 'Description for Result 2', url: 'https://example.com/result2' },
+        // Add more results as needed
+      ];
     },
-    methods: {
-      search() {
-        // Simulate search results (replace this with your actual data)
-        this.results = [
-          { id: 1, title: 'Result 1', description: 'Description for Result 1', url: 'https://example.com/result1' },
-          { id: 2, title: 'Result 2', description: 'Description for Result 2', url: 'https://example.com/result2' },
-          // Add more results as needed
-        ];
-      },
-    },
-  };
-  </script>
-  
-  <style scoped>
-  /* Add your component styles here */
-  
-  div {
-    max-width: 600px;
-    margin: auto;
-    padding: 20px;
-  }
-  
-  input {
-    width: 100%;
-    padding: 8px;
-    margin-bottom: 10px;
-  }
-  
-  button {
-    padding: 10px;
-    background-color: #4caf50;
-    color: white;
-    border: none;
-    cursor: pointer;
-  }
-  
-  button:hover {
-    background-color: #45a049;
-  }
-  
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-  
-  li {
-    margin-bottom: 20px;
-  }
-  
-  a {
-    text-decoration: none;
-    color: #1e88e5;
-    font-weight: bold;
-  }
-  
-  a:hover {
-    text-decoration: underline;
-  }
-  
-  </style>
+  },
+};
+</script>

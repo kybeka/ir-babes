788e4eca5d602ad5f348b0610f47244b6aaa33c2
kyla kaplan <kaplak@usi.ch>
2023-12-05 15:49:36 +0100

    added Vuetify

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,29 +1,23 @@
 <template>
-  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-  <SearchComponent/>
+  <v-app>
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-import SearchComponent from './components/SearchComponent.vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+
   components: {
-    // HelloWorld,
-    SearchComponent
-  }
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import vuetify from './plugins/vuetify'
+
+import { loadFonts } from './plugins/webfontloader'
+
+loadFonts()
+
+createApp(App)
+  .use(vuetify)
+  .mount('#app')
